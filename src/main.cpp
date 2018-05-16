877660b5278ae5f55f0bc55f996e8ab2ff6d1d3f
Kite's Runner <shengyao@umich.edu>
2018-05-15 21:23:47 -0400

    debug

--- src/main.cpp
+++ src/main.cpp
@@ -252,7 +252,7 @@ int main() {
               double next_d = 6;
               vector<double> xy = getXY(next_s, next_d, map_waypoints_s, map_waypoints_x, map_waypoints_y);
               next_x_vals.push_back(xy[0]);
-              next_x_vals.push_back(xy[1]);
+              next_y_vals.push_back(xy[1]);
               // next_x_vals.push_back(car_x+(dist_inc*i)*cos(deg2rad(car_yaw)));
               // next_y_vals.push_back(car_y+(dist_inc*i)*sin(deg2rad(car_yaw)));
             }
